6c3c594234b091d7fee2d3bd6d9f50f4f578e3c9
AFrenchWrench <AFrenchWrench@gmail.com>
2025-07-05 18:27:48 +0330

    added the solution for https://quera.org/problemset/2885

---
+++ easy/implementation/a simple question/a simple question.py
@@ -0,0 +1,10 @@
+# link https://quera.org/problemset/2885
+
+# with simple string multiplication
+lines = int(input())
+print("man khoshghlab hastam\n" * lines)
+
+# with a for loop:
+# lines = int(input())
+# for i in range(lines):
+#     print("man khoshghlab hastam")
