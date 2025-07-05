d61e4535d2a5a684fd7b333659e87eb1151a8e78
AFrenchWrench <AFrenchWrench@gmail.com>
2025-07-05 18:33:48 +0330

    added the solution for https://quera.org/problemset/589

---
+++ easy/programming fundamentals/factorial/factorial.py
@@ -0,0 +1,14 @@
+# link https://quera.org/problemset/589
+
+# with a for loop
+num = int(input())
+x = 1
+for i in range(2, num + 1):
+    x *= i
+print(x)
+
+# recursive functions
+# def factorial(n):
+#     return 1 if n < 2 else n * factorial(n - 1)
+# num = int(input())
+# print(factorial(num))
