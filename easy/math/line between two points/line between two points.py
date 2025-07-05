1cab1755e954f9cb232e08d5bc57f30021c23b11
AFrenchWrench <AFrenchWrench@gmail.com>
2025-07-05 19:57:53 +0330

    added the solution for https://quera.org/problemset/3414

---
+++ easy/math/line between two points/line between two points.py
@@ -0,0 +1,9 @@
+# link https://quera.org/problemset/3414
+
+x1, y1, x2, y2 = map(int, input().split())
+if x1 == x2:
+    print("Vertical")
+elif y1 == y2:
+    print("Horizontal")
+else:
+    print("Try again")
