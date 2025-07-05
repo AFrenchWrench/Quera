b355fd6eea480bffb77b50eac5f7650630c67101
AFrenchWrench <AFrenchWrench@gmail.com>
2025-07-05 18:23:49 +0330

    added the solution for https://quera.org/problemset/3537

---
+++ easy/implementation/yellow question/yellow question.py
@@ -0,0 +1,9 @@
+# link https://quera.org/problemset/3537
+
+# solution with f string
+intel = int(input())
+print(f"W{"o" * intel}w!")
+
+# if you don't know f string
+# intel = int(input())
+# print("W", "o" * intel, "w!", sep="")
