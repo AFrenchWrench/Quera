1615833393c871f5b0f8ba1afc708e3b39001726
AFrenchWrench <AFrenchWrench@gmail.com>
2025-07-10 18:51:42 +0330

    added the solution for https://quera.org/problemset/595

---
+++ easy/programming fundamentals/khayyam triangle/khayyam triangle.py
@@ -0,0 +1,20 @@
+# link https://quera.org/problemset/595
+
+
+def generate_pascals_triangle(n):
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            element = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row.append(element)
+        row.append(1)
+        triangle.append(row)
+    return triangle
+
+
+n = int(input())
+
+triangle = generate_pascals_triangle(n)
+for row in triangle:
+    print(" ".join(map(str, row)))
